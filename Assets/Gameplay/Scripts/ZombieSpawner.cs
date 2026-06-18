bf2f7dbb179fcd17e11067b154221f37afd7a486
jaydeep <jaydeep7624@gmail.com>
2026-06-18 14:45:05 +0530

    Zombie movement Work Complete with grass type play area

--- Assets/Gameplay/Scripts/ZombieSpawner.cs
+++ Assets/Gameplay/Scripts/ZombieSpawner.cs
@@ -1,1 +1,19 @@
-using UnityEngine; public class ZombieSpawner : MonoBehaviour { public GameObject zombiePrefab; public float spawnInterval = 1.5f; private void Start() { InvokeRepeating(nameof(SpawnZombie), 0.5f, spawnInterval); } private void SpawnZombie() { if (zombiePrefab != null) Instantiate(zombiePrefab, transform.position, Quaternion.identity); } }
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+    public GameObject zombiePrefab;
+    public float spawnInterval = 1.5f;
+    [SerializeField] private float[] laneYPositions = { 3.0f, 1.5f, 0.0f, -1.5f, -3.0f };
+
+    private void Start() {
+        InvokeRepeating(nameof(SpawnZombie), 0.5f, spawnInterval);
+    }
+
+    private void SpawnZombie() {
+        if (zombiePrefab != null && laneYPositions != null && laneYPositions.Length > 0) {
+            float randomY = laneYPositions[Random.Range(0, laneYPositions.Length)];
+            Vector3 spawnPosition = new Vector3(transform.position.x, randomY, transform.position.z);
+            Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
